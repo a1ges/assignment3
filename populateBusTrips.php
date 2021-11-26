23acb47beabdb082fe02619ed2d19c0e55aa4dfb
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 10:31:32 -0500

    Updated files

--- populateBusTrips.php
+++ populateBusTrips.php
@@ -1,6 +1,9 @@
 <?php 
+include 'connectdb.php';
 
-// This document is used to populate the 
-var checkedValue = myRadio.filter(":checked").val();
+// This document is used to populate the textboxes with predefined data which can be editted by the user
+      // get country name
+     $countryName = $_POST['countrySelect'];
+     echo $countryName;
 
 ?>

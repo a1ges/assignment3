fe7548d90ec151123323236b813b0bfc99e30e5b
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 09:27:10 -0500

    Updated files

--- home.php
+++ home.php
@@ -15,23 +15,52 @@
 include 'connectdb.php';
 ?>
 <h1>Welcome to the Western Bus Trip Editor</h1>
+
+<!-- Form segment, creating order radio buttons and calling upon the generation of the database -->
 <form name = "radioOptions" method="POST">
+<!-- Ascending Order Radio Button -->
 <tr> <td style="text-align:center"><input type="radio" id = "orderasc" name ="ordering"  value="orderasc" >
 <label for="orderasc"> Ascending Order </label>  </td>
+<!-- Country Order Radio Button -->
 <td style="text-align:center"><input type="radio" id = "tripnameOcountry" name ="tripnameOcountry"  value="visitedcountry" >
 <label for="orderasc"> Order by: Country names </label>  </td></tr>
-
+<!-- Descending Order Radio Button -->
 <br><tr> <td style="text-align:center"><input type="radio" id = "orderdesc" name="ordering"  value="orderdesc">
 <label for="orderdesc"> Descending Order </label></td>
+<!-- Trip Order Radio Button -->
 <td style="text-align:center"><input type="radio" id = "tripnameOcountry" name ="tripnameOcountry"  value="tripname" >
 <label for="orderasc"> Order by: Trip names </label>  </td></tr>
+
+<!--"Submit" button -->
 <br><input type="submit" value="submit"/>
 </form>
+<!--Call upon getTrips using radio button information -->
 <?php
 include 'getTrips.php'
-
 ?>
 
+<!-- Form Segment , takes values from radio button in previous form (look for countryvisisted) and populate texts boxes to be editted once the "Edit" Button is clicked -->
+
+<form>
+    <!-- Headings for row of inputs (Some edittable, some not)-->
+    <tr>    
+        <th>Trip Name</th>
+        <th>Country</th>"
+        <th>Start Date</th>
+        <th>End Date</th>"
+        <th>Bus License Number</th>
+        <th>Trip ID</th>"
+    </tr>
+    <tr>    
+        <td><input type="text" id="fname" name="fname"></td>
+        <td><input type="text" id="fname" name="fname"></td>"
+        <td><input type="text" id="fname" name="fname"></td>
+        <td><input type="text" id="fname" name="fname"></td>"
+        <td><input type="text" id="fname" name="fname"></td>
+        <td><input type="text" id="fname" name="fname"></td>"
+    </tr>
+    
+</form>
 </body>
 </html>
 

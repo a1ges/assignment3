f8cb65086cf35b31b1297bc4e36c14cf4e452aab
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 03:09:01 -0500

    Updated files

--- index2.php
+++ index2.php
@@ -15,62 +15,7 @@ include 'connectdb.php';
 <input type="radio" id = "orderdesc" name="ordering"  value="orderdesc">
 <label for="orderdesc"> Descending Order </label><br>
 </form>
-<?php
 
-
-// Selecting all elements from bustrips   
-$query = "SELECT * FROM bustrips";
-// if Orderasc radio button selected. Order by ascending tripname (work out for country as well)
-   if(isset($_GET['ordering'])){
-        $query .= " ORDER BY tripname ASC";
-        echo "<br> Ascending clicked! <br>"; 
-   };
-// same as previous line but descending
-   if(isset($_GET['ordering'])) $query .=" ORDER BY tripname DESC";
-   $result = mysqli_query($connection,$query) or die(mysqlerror());
-// if not dead result , print the table
-
-if($result->num_rows>0){
-echo "<table id = \"tripstable\">";
-echo "<tr>";
-echo "<th>Trip Name</th>";
-echo "<th>Country</th>";
-echo "<th>Start Date</th>";
-echo "<th>End Date</th>";
-echo "<th>Bus License Number</th>";
-echo "<th>Trip ID</th>";
-	while($row = $result->fetch_assoc()){
-     echo "<tr>";
-     echo "<td style=\"text-align:center\">";
-     echo $row["tripname"];
-     echo "</td>";
-     echo "<td style=\"text-align:center\">";
-     echo $row["visitedcountry"];
-     echo "</td>";
-     echo "<td style=\"text-align:center\">";
-     echo $row["startdate"];
-     echo "</td>";
-     echo "<td style=\"text-align:center\">";
-     echo $row["enddate"];
-     echo "</td>";
-     echo "<td style=\"text-align:center\">";
-     echo $row["licenseplate"];
-     echo "</td>";
-     echo "<td style=\"text-align:center\">";
-     echo $row["tripid"];
-     echo "</td>";
-     echo "</tr>";	
-}
-
-}
-else{
-	echo "Zero results :(";
-} 
-
-
-mysqli_free_result($result);
-mysqli_close($connection);
-?>
 </body>
 </html>
 

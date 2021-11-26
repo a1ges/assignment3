4faa9c69e91ce13ea83d416e7b429f967b5168af
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 17:15:39 -0500

    Updated files

--- home.php
+++ home.php
@@ -33,15 +33,45 @@ include 'connectdb.php';
 
 <!--"Submit" button -->
 <br><input type="submit" value="Order Table"/>
-</form>
-<!--Call upon getTrips using radio button information -->
 <?php
 include 'getTrips.php'
 ?>
+</form>
+<!--Call upon getTrips using radio button information -->
+
 
 <!-- Form Segment , takes values from radio button in previous form (look for countryvisisted) and populate texts boxes to be editted once the "Edit" Button is clicked -->
 
+<form name = "editBusTripForm" method = "POST">
+    <!-- Headings for row of inputs (Some edittable, some not)-->
+    <tr>    
+        <th></th>
+        <th>Trip Image</th>
+        <th>Trip Name</th>
+        <th>Country</th>"
+        <th>Start Date</th>
+        <th>End Date</th>"
+        <th>Bus License Number</th>
+        <th>Trip ID</th>"
+    </tr>
+    <tr>    
+        <!-- First table entry used to space out inputs for aesthetic reasons. -->
+        <td style="text-align:center"></td>
+        
+        <td style="text-align:center"><input type="text" id="urlimage" name="urlimage"></td>"
+        <td style="text-align:center"><input type="text" id="tripname" name="tripname"></td>
+        <td style="text-align:center"><input type="text" id="visitedcountry" name="visitedcountry" readonly style="background-color: grey;"></td>"
 
+        <!-- Note : Make sure start date < end date -->
+
+        <td style="text-align:center"><input type="date" id="startdate" name="startdate"></td>
+        <td style="text-align:center"><input type="date" id="enddate" name="enddate"></td>"
+
+        <td style="text-align:center"><input type="text" id="licenseplate" name="licenseplate"></td>"
+        <td style="text-align:center"><input type="text" id="trpid" name="tripid" readonly style = "background-color: grey;"></td>"
+        <td style="text-align:center"><input type="submit" value="Submit Edits"/></td>
+    </tr>  
+</form>
 <?php
 
 include 'editbustrip.php'

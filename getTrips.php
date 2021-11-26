668a9af57279958154b3be0b4820d4dc8ebc333c
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 10:24:01 -0500

    Updated files

--- getTrips.php
+++ getTrips.php
@@ -12,6 +12,10 @@ $query = "SELECT * FROM bustrips";
 // same as previous line but descending
    if($ordering == "orderdesc") $query .=" ORDER BY ". $tripOrCountryOrdering . " DESC";
    $result = mysqli_query($connection,$query) or die(mysqlerror());
+   }else{
+    
+      echo "<span class=\"popuptext\" id=\"myPopup\">Popup text...</span>";
+    
    }
 // if not dead result , print the table
 

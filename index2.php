7d3439af44dcde58fd697c6d8d3e575a5b265add
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 03:17:02 -0500

    Updated files

--- index2.php
+++ index2.php
@@ -3,13 +3,19 @@
 <head>
 <meta charset="utf-8">
 <title>Agent 69's Triptastic Editor!</title>
+<script>
+    function autoSubmit(){
+        var formObject = document.forms['radioOptions']
+        formObject.submit();
+    }
+    </script>
 </head>
 <body>
 <?php
 include 'connectdb.php';
 ?>
 <h1>Welcome to the Western Bus Trip Editor</h1>
-<form action="getdata.php" method="POST">
+<form name = "radioOptions" action="getdata.php" method="POST">
 <input type="radio" id = "orderasc" name ="ordering"  value="orderasc">
 <label for="orderasc"> Ascending Order </label><br>
 <input type="radio" id = "orderdesc" name="ordering"  value="orderdesc">

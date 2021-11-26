b4bdf83a174a49cc074498f9ecf134ea5bdc2c55
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 03:35:01 -0500

    Updated files

--- getdata.php
+++ getdata.php
@@ -4,10 +4,11 @@ include 'connectdb.php';
 // Selecting all elements from bustrips   
 $query = "SELECT * FROM bustrips";
 // if Orderasc radio button selected. Order by ascending tripname (work out for country as well)
-   if(isset($_GET['ordering'])== "orderdasc") $query .= " ORDER BY tripname ASC";
+   $input = $_POST['ordering'];
+   if($input == "orderasc") $query .= " ORDER BY tripname ASC";
 
 // same as previous line but descending
-   if(isset($_GET['ordering']) == "orderdesc") $query .=" ORDER BY tripname DESC";
+   if($input == "orderdesc") $query .=" ORDER BY tripname DESC";
    $result = mysqli_query($connection,$query) or die(mysqlerror());
 // if not dead result , print the table
 

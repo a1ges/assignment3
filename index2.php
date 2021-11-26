52fa93c242ad5004767eaf4981199bab163d819d
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 03:26:54 -0500

    Updated files

--- index2.php
+++ index2.php
@@ -16,9 +16,9 @@ include 'connectdb.php';
 ?>
 <h1>Welcome to the Western Bus Trip Editor</h1>
 <form name = "radioOptions" method="POST">
-<input type="radio" id = "orderasc" name ="ordering"  value="orderasc" onChange="autoSubmit();">
+<input type="radio" id = "orderasc" name ="ordering"  value="orderasc" >
 <label for="orderasc"> Ascending Order </label><br>
-<input type="radio" id = "orderdesc" name="ordering"  value="orderdesc" onChange="autoSubmit();">
+<input type="radio" id = "orderdesc" name="ordering"  value="orderdesc">
 <label for="orderdesc"> Descending Order </label><br>
 </form>
 <?php

5614ee2e59595382117d79993d969189f311be90
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 10:32:53 -0500

    Updated files

--- populateBusTrips.php
+++ populateBusTrips.php
@@ -4,6 +4,5 @@ include 'connectdb.php';
 // This document is used to populate the textboxes with predefined data which can be editted by the user
       // get country name
      $countryName = $_POST['countrySelect'];
-     echo $countryName;
 
 ?>

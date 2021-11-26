e83f7dc457f6dc609cb8de847f50b4ed5ff54475
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-26 10:22:26 -0500

    Updated files

--- getTrips.php
+++ getTrips.php
@@ -6,11 +6,13 @@ $query = "SELECT * FROM bustrips";
 // if Orderasc radio button selected. Order by ascending tripname (work out for country as well)
    $ordering = $_POST['ordering'];
    $tripOrCountryOrdering = $_POST['tripnameOcountry'];
+   if($tripOrCountryOrdering != NULL){
    if($ordering == "orderasc") $query .= " ORDER BY ". $tripOrCountryOrdering . " ASC";
 
 // same as previous line but descending
    if($ordering == "orderdesc") $query .=" ORDER BY ". $tripOrCountryOrdering . " DESC";
    $result = mysqli_query($connection,$query) or die(mysqlerror());
+   }
 // if not dead result , print the table
 
 if($result->num_rows>0){

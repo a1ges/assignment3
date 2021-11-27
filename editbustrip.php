e53bfb979232b93f34b282f3834437958c6f1fd4
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 12:31:24 -0500

    Updated files

--- editbustrip.php
+++ editbustrip.php
@@ -1,7 +1,7 @@
 <?php
 include 'connectdb.php';
 // get values from textboxes
-if(isset(_POST['submitEdit'])){
+
 $urlimage = $_POST['urlimage'];
 $tripname = $_POST['tripname']; // countryvisited
 $startdate = $_POST['startdate'];
@@ -45,6 +45,6 @@ $preceeded = false;
     }
     else{
         echo "<script>alert('Edit Complete');</script>";
-    }
+    
 }
 ?>

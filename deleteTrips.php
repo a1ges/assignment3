ad1d805cb64af5c65e776b86a725428b82ab27a4
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 10:11:06 -0500

    Updated files

--- deleteTrips.php
+++ deleteTrips.php
@@ -5,27 +5,13 @@
 if(isset($delete)){
    $tripid = $_POST["tripid"];
    if($tripid != NULL){
-   //$query1= "DELETE * FROM bustrips where tripid = ". $tripid ;
-   //$result=mysqli_query($connection,$query1);
+   $query1= "DELETE * FROM bustrips where tripid = ". $tripid ;
+   $result=mysqli_query($connection,$query1);
    if (!$result) {
-          die("Query failed");
-   }else{
-
+    echo "Cannot Delete";
    }
-   $row=mysqli_fetch_assoc($result);
-   
-   $delete_row_query = ""
-   //if (!mysqli_query($connection, $query)) {
-      //  die("Error: insert failed" . mysqli_error($connection));
-    //}
-   
   
-   //} //else {
-       //echo "Invalid deletion request";
-   //}
+}
 }
 mysqli_close($connection);
 ?>
-</ol>
-</body>
-</html>

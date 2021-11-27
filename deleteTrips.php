d178b3d521524c5a01a503cf36de94b414ea0c3d
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 10:14:11 -0500

    Updated files

--- deleteTrips.php
+++ deleteTrips.php
@@ -2,13 +2,13 @@
    include 'connectdb.php';
 
    $delete= $_POST["delete"];
-if(isset($delete)){
+if(isset($tripid)){
    $tripid = $_POST["tripid"];
    if($tripid != NULL){
-   $query1= "DELETE * FROM bustrips where tripid = ". $tripid ;
+   $query1= "DELETE FROM bustrips where tripid = ". $tripid ;
    $result=mysqli_query($connection,$query1);
    if (!$result) {
-    echo "Cannot Delete";
+    echo '<script>alert("This Bus Trip cannot be deleted, there are constraints.")</script>';
    }
   
 }

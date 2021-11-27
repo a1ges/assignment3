31bf7efb860db62f36b63fcbefe492364905bb94
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 10:30:36 -0500

    Updated files

--- deleteTrips.php
+++ deleteTrips.php
@@ -10,8 +10,9 @@
    $query1= "DELETE FROM bustrips where tripid = ". $tripid ;
    $result=mysqli_query($connection,$query1);
    if (!$result) {
-    die();
     echo "<script>alert('Deletion cannot be completed. Constraint Error.');</script>";
+    die();
+    
    }
   
 }

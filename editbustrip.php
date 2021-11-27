10fdf5e1eb9f681c4a37aa8f6d7dc0b54486e646
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 11:58:43 -0500

    Updated files

--- editbustrip.php
+++ editbustrip.php
@@ -8,6 +8,12 @@ $startdate = $_POST['startdate'];
 $enddate = $_POST['enddate'];
 $tripid = $_POST['tripid'];
     echo "<h1>". $tripid. "</h1>";
-
-
+    $query1= "UPDATE bustrips SET tripname = \'". $tripname."\' , startdate =\'". $startdate ."\' , enddate =\'". $enddate . "\'  where tripid = ". $tripid ;
+    $result=mysqli_query($connection,$query1);
+    if (!$result) {
+        die("database edit failed.");
+    }
+    else{
+        echo "<script>alert('Edit Complete');</script>";
+    }
 ?>

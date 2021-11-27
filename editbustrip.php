9a9aae965990d42126260b56e494b3b9ae57071a
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 12:24:11 -0500

    Updated files

--- editbustrip.php
+++ editbustrip.php
@@ -6,31 +6,35 @@ $urlimage = $_POST['urlimage'];
 $tripname = $_POST['tripname']; // countryvisited
 $startdate = $_POST['startdate'];
 $enddate = $_POST['enddate'];
+if($startdate > $enddate){
+    $temp = $startdate;
+    $startdate = $enddate;
+    $enddate = $temp;
+}
 $tripid = $_POST['tripid'];
 
 $preceeded = false;
     echo "<h1>". $tripid. "</h1>";
     $query1= "UPDATE bustrips SET " ;// where tripid = ". $tripid ;\
     if($tripname != NULL){
-        $query1 .=  "tripname =\'".$tripname."\'";
+        $query1 .=  "tripname ='".$tripname."'";
         $preceeded =true;
     }
     if($startdate != NULL){
         if($preceeded == true){ $query1 .= ", ";}
-        $query1 .= "startdate =". $startdate ." , enddate =". $enddate . "\'";
+        $query1 .= "startdate =". $startdate ;
         $preceeded == true;
         
     }
     if($enddate != NULL){
         if($preceeded = true){ $query1 .= ", ";}
-
-        $end = 'enddate';
+        $query1 .= "enddate =". $enddate . "'";
         $preceeded == true;
     
     }
     
     if($preceeded = true){ $query1 .= ", ";}
-    $query1 .= "urlimage = \'". $urlimage . "\' WHERE tripid =". $tripid;
+    $query1 .= "urlimage = '". $urlimage . "    ' WHERE tripid =". $tripid;
     echo "<h1>" . $query1 . "</h1>";
     $result=mysqli_query($connection,$query1);
     if (!$result) {

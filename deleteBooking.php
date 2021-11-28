9ee5dff03b03d4e79e6fa0b3d01b1e563fd9ba3d
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 23:00:39 -0500

    Updated files

--- deleteBooking.php
+++ deleteBooking.php
@@ -5,9 +5,22 @@ $bookingid = $_POST['deleteBookingRadio'];
 $firstnameDelim = $_POST['deleteBooking'];
 
 
-$deletion = "DELETE FROM bookings where price in (select * from (select price from trips, bookings where trips.tripID = bookings.tripID AND tripname = '".$tripName."') tblTmp) AND price = ".$price;
+$query1 = "DELETE FROM bookings where price in (select * from (select price from trips, bookings where trips.tripID = bookings.tripID AND tripname = '".$tripName."') tblTmp) AND price = ".$price;
+
+$result=mysqli_query($connection,$query1);
+   if (!$result) {
+    echo "<script>alert('Deletion cannot be completed. Constraint Error.');</script>";
+    }
+    
+   
+  
 
 
+
+/*
+
+*/
+mysqli_close($connection);
 
 
 

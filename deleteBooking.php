747ff6a65889a4f7f6a32657f1da1f52a097f7ad
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 22:58:34 -0500

    Updated files

--- deleteBooking.php
+++ deleteBooking.php
@@ -1,11 +1,11 @@
 <?php
 
-
+echo "<script>alert('Deleting Booking');</script>";
 $bookingid = $_POST['deleteBookingRadio'];
 $firstnameDelim = $_POST['deleteBooking'];
 
 
-echo $firstnameDelim  . " " .$bookingid;
+$deletion = "DELETE FROM bookings where price in (select * from (select price from trips, bookings where trips.tripID = bookings.tripID AND tripname = '".$tripName."') tblTmp) AND price = ".$price;
 
 
 

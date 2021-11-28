36b98ec6f5456df13b401af760956b2e1c8f3270
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 19:01:25 -0500

    Updated files

--- booktrip.php
+++ booktrip.php
@@ -2,12 +2,13 @@
 
 include 'connectdb.php';
 
-$query = "INSERT INTO bookings";
+$query = "INSERT INTO bookings VALUES(";
 $passengerid = $_POST['passengerid'];
 $tripid = $_POST['tripid'];
 $price = $_POST['price'];
 
 if($passengerid != NULL && $tripid!=NULL){
+    $query .= $passengerid . " , " . $tripid . " , " . $price . ")";
     $result = mysqli_query($connection,$query) ;
     if (!$result) {
         die("<script>alert('Invalid query please try again'); window.location.reload();</script>");

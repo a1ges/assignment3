f038a5b22c9e8c470c73b3edb38c1a177140a23b
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 20:04:07 -0500

    Updated files

--- showPassengerBookings.php
+++ showPassengerBookings.php
@@ -6,9 +6,63 @@ $passengerid = $_POST['passengerid'];
 if(isset($submitted) && isset($submitted)){
 
 
-$query = "SELECT * from bookings INNER JOIN passenger ON bookings.passengerid = passenger.passengerid WHERE bookings.passengerid = \"" . $passengerid . "\"";
+$query = "SELECT * from bookings INNER JOIN passenger ON bookings.passengerid = passenger.passengerid INNER JOIN bustrips ON bookings.tripid = bustrips.tripid WHERE bookings.passengerid = \"" . $passengerid . "\"";
 
-echo $query;
+// debugging: echo $query;
+$result = mysqli_query($connection,$query) or die(mysqlerror());
+if($result->num_rows>0){
+  
+    echo "<table id = \"passengerBookingTable\">";
+ echo "<tr>";
+ echo "<th> Select </th>";
+ echo "<th> First Name</th>";
+ echo "<th> Last Name</th>";
+ echo "<th>Passport Number</th>";
+ echo "<th>Country</th>";
+ echo "<th>Start Date</th>";
+ echo "<th>End Date</th>";
+ echo "<th>Bus License Number</th>";
+ echo "<th>Trip ID</th>";
+     while($row = $result->fetch_assoc()){
+      echo "<tr>";
+      // add radio button for easier country editting
+      
+      echo "<td style=\"text-align:center\">";
+      echo "<input type=\"radio\" id = \"deleteBookingRadio\" name =\"deleteBookingRadio\" value=". $row['tripid']. " width=\"100\" height=\"100\" >";
+      echo "</td>";
+      
+      echo "<td style=\"text-align:center\">";
+      if($row["urlimage"] != NULL){
+      echo "<img src=". $row["urlimage"] ." width=\"100\" height=\"100\">";
+      }else{
+       echo "<img src= https://icon-library.com/images/null-icon/null-icon-1.jpg width=\"100\" height=\"100\"> ";
+      }
+      echo "</td>";
+      echo "<td style=\"text-align:center\">";
+      echo $row["tripname"];
+      echo "</td>";
+      echo "<td style=\"text-align:center\">";
+      echo $row["visitedcountry"];
+      echo "</td>";
+      echo "<td style=\"text-align:center\">";
+      echo $row["startdate"];
+      echo "</td>";
+      echo "<td style=\"text-align:center\">";
+      echo $row["enddate"];
+      echo "<td style=\"text-align:center\">";
+      echo $row["tripid"];
+      echo "</td>";
+      
+      echo "</tr>";	
+ }
+ echo "<td style=\"text-align:center\"><input type=\"submit\" name = \"delete\" value=\"Delete Selected\" onclick=\"return confirm('Are you sure?');\"/></td>";
+ echo "</table>";
+ 
+ }
+ else{
+     echo "Zero results :(";
+ } 
+ 
 
 }
 mysqli_close($connection);

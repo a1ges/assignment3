b53781f19e383405458da619954041aae0aed087
マルツ <aegis@debian-BULLSEYE-live-builder-AMD64>
2021-11-27 19:02:11 -0500

    Updated files

--- booktrip.php
+++ booktrip.php
@@ -9,6 +9,7 @@ $price = $_POST['price'];
 
 if($passengerid != NULL && $tripid!=NULL){
     $query .= $passengerid . " , " . $tripid . " , " . $price . ")";
+    echo "<h1>". $query . "<h1>";
     $result = mysqli_query($connection,$query) ;
     if (!$result) {
         die("<script>alert('Invalid query please try again'); window.location.reload();</script>");
